0e271f6283c2cfae5101849bc8d70268d8f55d24
Rohhelion <kirorei04@gmail.com>
2023-06-19 00:41:22 +0300

    deleted package.lock,json

---
+++ challange-2.js
@@ -0,0 +1,22 @@
+function checkSpeed(speeed){
+    const speedlimit = 70
+    const points = 5
+    const pointsthreshhold = 12
+    if (speed <= speeedlimit){
+        return "Okay"
+
+    }else {
+        const exessspeed =speed - speedlimit
+        const points =Math.floor(speed / points)
+        if (points > pointsthreshhold){
+    return "SUSPENDED LICENCE"
+
+        }else {
+            return "points" + points
+        }
+
+    } 
+}
+const speed =parseInt ("100")//ENTER SPEED
+checkSpeed(speed)
+console.log(checkSpeed(speed))
